76d6f1ae9b452b821babc9fe6c9987ffdc269dd2
Rony <rony.ahmed9771@gmail.com>
2021-03-21 23:17:17 +0600

    Final lab1

---
+++ FLab1/FLab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLab1
+{
+    class Program
+    {
+        Account a1 = new Account("Ahmed", "001", 2000.0);
+        Account a2 = new Account("Rony", "002", 3500.0);
+
+        Transaction t1 = new Transaction(a1, a2, 100);
+
+
+
+
+
+    }
+}
